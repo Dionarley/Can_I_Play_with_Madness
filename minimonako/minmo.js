8edac7bd9750f95ce50b1f1a2ee7c5da9d797886
Dionarley <dionarley@gmail.com>
2025-03-22 17:26:41 -0300

    local index for init service test

--- minimonako/minmo.js
+++ minimonako/minmo.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
-})
-
-app.listen(port, () => {
-  console.log(`App de exemplo esta rodando na porta ${port}`)
-})
+require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@0.21.2/min/vs' }});
+require(['vs/editor/editor.main'], function() {
+    var editor = monaco.editor.create(document.getElementById('container'), {
+        value: '// Comece a digitar seu código aqui...',
+        language: 'javascript',
+        theme: 'vs-dark' // tema opcional
+    });
+});
